ca4edbc5bbd300f04e6c8a801899a76a5818a529
Arif Setiawan <arifswn1@gmail.com>
2024-09-12 20:14:22 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// check aritmatika
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero");
+  }
+  return a / b;
+}
+
+// check function
+function isNameValid(name) {
+  return /^[a-zA-Z]+$/.test(name);
+}
+
+// check REST Api
+function getAPI(url, callback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error("Error fetching data: " + error));
+}
